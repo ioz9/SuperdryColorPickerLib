5482f18aa1027790d36596804b2b2c8829b1ad1c
Daniel Huckaby <dhuckaby@mercury.io>
2012-06-03 10:15:08 -0500

    Cleaned up ColorPickerPreference

--- src/org/superdry/util/colorpicker/lib/ColorPickerPreference.java
+++ src/org/superdry/util/colorpicker/lib/ColorPickerPreference.java
@@ -12,64 +12,59 @@ import android.widget.LinearLayout;
 
 public class ColorPickerPreference extends Preference {
 
-	private int color = 0xff000000;
-	private int borderViewWidth;
+    private int mColor = Color.BLACK;
+    private int mBorderViewWidth;
 
-	public ColorPickerPreference(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		if (this.getWidgetLayoutResource() != R.layout.prefcolorview) {
-			setWidgetLayoutResource(R.layout.prefcolorview);
-		}
+    public ColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWidgetLayoutResource(R.layout.prefcolorview);
+    }
 
-	}
+    public ColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setWidgetLayoutResource(R.layout.prefcolorview);
+    }
 
-	public ColorPickerPreference(Context context, AttributeSet attrs,
-			int defStyle) {
-		super(context, attrs, defStyle);
-		if (this.getWidgetLayoutResource() != R.layout.prefcolorview) {
-			setWidgetLayoutResource(R.layout.prefcolorview);
-		}
-	}
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        LinearLayout linearlayout = (LinearLayout) view.findViewById(R.id.prefcolorlayout);
+        if (linearlayout.getChildCount() == 0) {
+            linearlayout.addView(new BorderView(getContext(), mColor, mBorderViewWidth), mBorderViewWidth, mBorderViewWidth);
+        }
+    }
 
-	protected void onBindView(View view) {
-		super.onBindView(view);
-		LinearLayout linearlayout = (LinearLayout) view
-				.findViewById(R.id.prefcolorlayout);
-		if (linearlayout.getChildCount() == 0) {
-			linearlayout.addView(new BorderView(getContext()), borderViewWidth,
-					borderViewWidth);
-		}
-	}
+    public void setColor(int color) {
+        mColor = color;
+    }
 
-	public class BorderView extends View {
-		public BorderView(Context context) {
-			super(context);
-		}
+    private static class BorderView extends View {
 
-		protected void onDraw(Canvas canvas) {
-			Paint paint = new Paint();
-			paint.setAntiAlias(true);
-			paint.setStyle(Paint.Style.FILL);
-			paint.setColor(Color.WHITE);
-			canvas.drawRoundRect(new RectF(0, 0, borderViewWidth,
-					borderViewWidth), 3, 3, paint);
-			paint.setStyle(Paint.Style.FILL);
-			paint.setColor(Color.BLACK);
-			canvas.drawRoundRect(new RectF(1, 1, borderViewWidth - 1,
-					borderViewWidth - 1), 3, 3, paint);
-			paint.setStyle(Paint.Style.FILL);
-			paint.setColor(color);
-			canvas.drawRoundRect(new RectF(2, 2, borderViewWidth - 2,
-					borderViewWidth - 2), 3, 3, paint);
-		}
+        private int mBorderViewWidth;
+        private int mColor;
+        private Paint mPaint = new Paint();
 
-	}
+        public BorderView(Context context, int color, int boarderViewWidth) {
+            super(context);
+            mBorderViewWidth = boarderViewWidth;
+            mColor = color;
+            mPaint.setAntiAlias(true);
+        }
 
-	public void setDpi(int dpi) {
-		this.borderViewWidth = (int) (dpi * 75 / 240);
-	}
-
-	public void setColor(int color) {
-		this.color = color;
-	}
+        /**
+         * TODO Reuse RectF, object creation during onDraw is very expensive
+         */
+        @Override
+        protected void onDraw(Canvas canvas) {
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(Color.WHITE);
+            canvas.drawRoundRect(new RectF(0, 0, mBorderViewWidth, mBorderViewWidth), 3, 3, mPaint);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(Color.BLACK);
+            canvas.drawRoundRect(new RectF(1, 1, mBorderViewWidth - 1, mBorderViewWidth - 1), 3, 3, mPaint);
+            mPaint.setStyle(Paint.Style.FILL);
+            mPaint.setColor(mColor);
+            canvas.drawRoundRect(new RectF(2, 2, mBorderViewWidth - 2, mBorderViewWidth - 2), 3, 3, mPaint);
+        }
+    }
 }
