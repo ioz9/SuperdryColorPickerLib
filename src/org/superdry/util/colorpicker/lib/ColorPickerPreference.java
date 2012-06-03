dccb3c38b7f9962afe2afba865524e511d3923aa
Daniel Huckaby <dhuckaby@mercury.io>
2012-06-03 11:06:05 -0500

    Finished merge of Preference impl

--- src/org/superdry/util/colorpicker/lib/ColorPickerPreference.java
+++ src/org/superdry/util/colorpicker/lib/ColorPickerPreference.java
@@ -1,10 +1,13 @@
 package org.superdry.util.colorpicker.lib;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Bundle;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,17 +15,42 @@ import android.widget.LinearLayout;
 
 public class ColorPickerPreference extends Preference {
 
-    private int mColor = Color.BLACK;
+    private int mColor;
     private int mBorderViewWidth;
+    private SupportActivity mSupportActivity;
+
+    public ColorPickerPreference(Context context) {
+        super(context);
+        setup(context, null, 0);
+    }
 
     public ColorPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setWidgetLayoutResource(R.layout.prefcolorview);
+        setup(context, attrs, 0);
     }
 
     public ColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setup(context, attrs, defStyle);
+    }
+
+    private void setup(final Context context, AttributeSet attrs, int defStyle) {
         setWidgetLayoutResource(R.layout.prefcolorview);
+
+        setColor(getPersistedInt(Color.BLUE));
+
+        mBorderViewWidth = context.getResources().getDimensionPixelSize(R.dimen.border_width);
+
+        setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(context, SuperdryColorPicker.class);
+                intent.putExtra(SuperdryColorPicker.EXTRA_INITIAL_COLOR, mColor);
+                if (mSupportActivity != null) {
+                    mSupportActivity.startActivityForResult(intent, SuperdryColorPicker.ACTION_GETCOLOR);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -34,8 +62,32 @@ public class ColorPickerPreference extends Preference {
         }
     }
 
+    public void setSupportActivity(SupportActivity supportActivity) {
+        mSupportActivity = supportActivity;
+        mSupportActivity.setOnActivityResultListener(new OnActivityResultListener() {
+
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+                if (requestCode == SuperdryColorPicker.ACTION_GETCOLOR) {
+                    if (resultCode == Activity.RESULT_OK) {
+                        Bundle b = intent.getExtras();
+                        if (b != null) {
+                            setColor(b.getInt(SuperdryColorPicker.EXTRA_INITIAL_COLOR));
+                        }
+                    }
+                }
+            }
+        });
+    }
+
     public void setColor(int color) {
         mColor = color;
+        setSummary(color2String(mColor));
+        persistInt(mColor);
+    }
+
+    private String color2String(int color) {
+        return String.format("#%02x%02x%02x", Color.red(color), Color.green(color), Color.blue(color));
     }
 
     private static class BorderView extends View {
@@ -66,5 +118,17 @@ public class ColorPickerPreference extends Preference {
             mPaint.setColor(mColor);
             canvas.drawRoundRect(new RectF(2, 2, mBorderViewWidth - 2, mBorderViewWidth - 2), 3, 3, mPaint);
         }
+    }
+
+    public static interface SupportActivity {
+
+        public void startActivityForResult(Intent intent, int requestCode);
+
+        public void setOnActivityResultListener(OnActivityResultListener listener);
+    }
+
+    public static interface OnActivityResultListener {
+
+        public void onActivityResult(int requestCode, int resultCode, Intent intent);
     }
 }
--- src/org/superdry/util/colorpicker/lib/SuperdryColorPicker.java
+++ src/org/superdry/util/colorpicker/lib/SuperdryColorPicker.java
@@ -22,7 +22,6 @@
  * THE SOFTWARE.
  */
 
-
 package org.superdry.util.colorpicker.lib;
 
 import android.os.Bundle;
@@ -34,150 +33,131 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class SuperdryColorPicker extends Activity implements
-		ColorPickHSVCallback {
+public class SuperdryColorPicker extends Activity implements ColorPickHSVCallback {
 
-	private int mInitialColor;
-	private int mSelectedColor;
-	private TextView colorCode;
-	private TextView colorRGB;
-	private TextView colorHSV;
-	private TextView colorYUV;
-	private View pColor;
-	private View nColor;
-	private SuperdryColorPickerView colorView;
-	private Button ok;
-	private Button ng;
+    public static final String EXTRA_INITIAL_COLOR = "SelectedColor";
+    public static final int ACTION_GETCOLOR = 1;
 
-	public static final int ACTION_GETCOLOR = 1;
+    private int mInitialColor;
+    private int mSelectedColor;
+    private TextView colorCode;
+    private TextView colorRGB;
+    private TextView colorHSV;
+    private TextView colorYUV;
+    private View pColor;
+    private View nColor;
+    private SuperdryColorPickerView colorView;
+    private Button ok;
+    private Button ng;
 
-	private float[] HSVcolorCode;
-	private int[] YUVcolorCode;
-	private int dpi = 0;
+    private float[] HSVcolorCode;
+    private int[] YUVcolorCode;
+    private int dpi = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		Intent intent = getIntent();
-		Bundle b = intent.getExtras();
-		mInitialColor = b.getInt("SelectedColor");
-		HSVcolorCode = new float[3];
-		YUVcolorCode = new int[3];
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        mInitialColor = b.getInt(EXTRA_INITIAL_COLOR);
+        HSVcolorCode = new float[3];
+        YUVcolorCode = new int[3];
 
-		setContentView(R.layout.colorpickerlayout);
-		setTitle(R.string.app_name);
-		if (dpi == 0) {
-			DisplayMetrics metrics = new DisplayMetrics();
-			this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-			dpi = metrics.densityDpi;
-		}
-		colorView = (SuperdryColorPickerView) findViewById(R.id.colorView);
-		colorView.setColorCallback(this, mInitialColor, dpi);
+        setContentView(R.layout.colorpickerlayout);
+        setTitle(R.string.app_name);
+        if (dpi == 0) {
+            DisplayMetrics metrics = new DisplayMetrics();
+            this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            dpi = metrics.densityDpi;
+        }
+        colorView = (SuperdryColorPickerView) findViewById(R.id.colorView);
+        colorView.setColorCallback(this, mInitialColor, dpi);
 
-		colorCode = (TextView) findViewById(R.id.colorCode);
-		colorCode.setText(String.format("#%02x%02x%02x",
-				Color.red(mInitialColor), Color.green(mInitialColor),
-				Color.blue(mInitialColor)));
+        colorCode = (TextView) findViewById(R.id.colorCode);
+        colorCode.setText(String.format("#%02x%02x%02x", Color.red(mInitialColor), Color.green(mInitialColor), Color.blue(mInitialColor)));
 
-		colorRGB = (TextView) findViewById(R.id.colorRGB);
-		colorRGB.setText(String.format("RGB: R=%03d,G=%03d,B=%03d",
-				Color.red(mInitialColor), Color.green(mInitialColor),
-				Color.blue(mInitialColor)));
+        colorRGB = (TextView) findViewById(R.id.colorRGB);
+        colorRGB.setText(String.format("RGB: R=%03d,G=%03d,B=%03d", Color.red(mInitialColor), Color.green(mInitialColor), Color.blue(mInitialColor)));
 
-		Color.colorToHSV(mInitialColor, HSVcolorCode);
-		colorHSV = (TextView) findViewById(R.id.colorHSV);
-		colorHSV.setText(String.format("HSV: H=%03d,S=%03d,V=%03d",
-				(int) HSVcolorCode[0], (int) (HSVcolorCode[1] * 100.0f),
-				(int) (HSVcolorCode[2] * 100.0f)));
+        Color.colorToHSV(mInitialColor, HSVcolorCode);
+        colorHSV = (TextView) findViewById(R.id.colorHSV);
+        colorHSV.setText(String.format("HSV: H=%03d,S=%03d,V=%03d", (int) HSVcolorCode[0], (int) (HSVcolorCode[1] * 100.0f), (int) (HSVcolorCode[2] * 100.0f)));
 
-		YUVcolorCode = convertRGB2YUV(mInitialColor);
-		colorYUV = (TextView) findViewById(R.id.colorYUV);
-		colorYUV.setText(String.format("YUV: Y=%03d,U=%03d,V=%03d",
-				YUVcolorCode[0], YUVcolorCode[1], YUVcolorCode[2]));
+        YUVcolorCode = convertRGB2YUV(mInitialColor);
+        colorYUV = (TextView) findViewById(R.id.colorYUV);
+        colorYUV.setText(String.format("YUV: Y=%03d,U=%03d,V=%03d", YUVcolorCode[0], YUVcolorCode[1], YUVcolorCode[2]));
 
-		pColor = (View) findViewById(R.id.pColor);
-		pColor.setBackgroundColor(mInitialColor);
-		nColor = (View) findViewById(R.id.nColor);
-		nColor.setBackgroundColor(mInitialColor);
-		mSelectedColor = mInitialColor;
-		ok = (Button) findViewById(R.id.ok);
-		ng = (Button) findViewById(R.id.ng);
-		ok.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.putExtra("SelectedColor", mSelectedColor);
-				setResult(RESULT_OK, intent);
-				finish();
-			}
-		});
-		ng.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
+        pColor = (View) findViewById(R.id.pColor);
+        pColor.setBackgroundColor(mInitialColor);
+        nColor = (View) findViewById(R.id.nColor);
+        nColor.setBackgroundColor(mInitialColor);
+        mSelectedColor = mInitialColor;
+        ok = (Button) findViewById(R.id.ok);
+        ng = (Button) findViewById(R.id.ng);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_INITIAL_COLOR, mSelectedColor);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        ng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-	}
+    }
 
-	@Override
-	public void onChangeColor(float[] hsv) {
-		// TODO Auto-generated method stub
-		mSelectedColor = Color.HSVToColor(hsv);
-		colorCode.setText(String.format("#%02x%02x%02x",
-				Color.red(mSelectedColor), Color.green(mSelectedColor),
-				Color.blue(mSelectedColor)));
-		colorRGB.setText(String.format("RGB: R=%03d,G=%03d,B=%03d",
-				Color.red(mSelectedColor), Color.green(mSelectedColor),
-				Color.blue(mSelectedColor)));
+    @Override
+    public void onChangeColor(float[] hsv) {
+        mSelectedColor = Color.HSVToColor(hsv);
+        colorCode.setText(String.format("#%02x%02x%02x", Color.red(mSelectedColor), Color.green(mSelectedColor), Color.blue(mSelectedColor)));
+        colorRGB.setText(String.format("RGB: R=%03d,G=%03d,B=%03d", Color.red(mSelectedColor), Color.green(mSelectedColor), Color.blue(mSelectedColor)));
 
-		nColor.setBackgroundColor(mSelectedColor);
-		nColor.invalidate();
+        nColor.setBackgroundColor(mSelectedColor);
+        nColor.invalidate();
 
-		colorHSV.setText(String.format("HSV: H=%03d,S=%03d,V=%03d",
-				(int) hsv[0], (int) (hsv[1] * 100.0f), (int) (hsv[2] * 100.0f)));
+        colorHSV.setText(String.format("HSV: H=%03d,S=%03d,V=%03d", (int) hsv[0], (int) (hsv[1] * 100.0f), (int) (hsv[2] * 100.0f)));
 
-		YUVcolorCode = convertRGB2YUV(mSelectedColor);
-		colorYUV.setText(String.format("YUV: Y=%03d,U=%03d,V=%03d",
-				YUVcolorCode[0], YUVcolorCode[1], YUVcolorCode[2]));
+        YUVcolorCode = convertRGB2YUV(mSelectedColor);
+        colorYUV.setText(String.format("YUV: Y=%03d,U=%03d,V=%03d", YUVcolorCode[0], YUVcolorCode[1], YUVcolorCode[2]));
+    }
 
-	}
+    private int[] convertRGB2YUV(int color) {
+        ColorMatrix cm = new ColorMatrix();
+        cm.setRGB2YUV();
+        final float[] yuvArray = cm.getArray();
 
-	private int[] convertRGB2YUV(int color) {
-		ColorMatrix cm = new ColorMatrix();
-		cm.setRGB2YUV();
-		final float[] yuvArray = cm.getArray();
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+        int[] result = new int[3];
+        result[0] = floatToByte(yuvArray[0] * r + yuvArray[1] * g + yuvArray[2] * b);
+        result[1] = floatToByte(yuvArray[5] * r + yuvArray[6] * g + yuvArray[7] * b) + 127;
+        result[2] = floatToByte(yuvArray[10] * r + yuvArray[11] * g + yuvArray[12] * b) + 127;
+        return result;
+    }
 
-		int r = Color.red(color);
-		int g = Color.green(color);
-		int b = Color.blue(color);
-		int[] result = new int[3];
-		result[0] = floatToByte(yuvArray[0] * r + yuvArray[1] * g + yuvArray[2]
-				* b);
-		result[1] = floatToByte(yuvArray[5] * r + yuvArray[6] * g + yuvArray[7]
-				* b) + 127;
-		result[2] = floatToByte(yuvArray[10] * r + yuvArray[11] * g
-				+ yuvArray[12] * b) + 127;
-		return result;
-	}
+    private int floatToByte(float x) {
+        return Math.round(x);
+    }
 
-	private int floatToByte(float x) {
-		int n = java.lang.Math.round(x);
-		return n;
-	}
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        mSelectedColor = savedInstanceState.getInt(EXTRA_INITIAL_COLOR);
+        colorView.setColorCallback(this, mSelectedColor, dpi);
+        nColor.setBackgroundColor(mSelectedColor);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		mSelectedColor = savedInstanceState.getInt("SelectedColor");
-		colorView.setColorCallback(this, mSelectedColor, dpi);
-		nColor.setBackgroundColor(mSelectedColor);
-		super.onRestoreInstanceState(savedInstanceState);
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putInt("SelectedColor", mSelectedColor);
-		super.onSaveInstanceState(outState);
-	}
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(EXTRA_INITIAL_COLOR, mSelectedColor);
+        super.onSaveInstanceState(outState);
+    }
 }
